5a8cef728a4616ce08978bb4630ceff565ff9f5e
Shuvayu Dhar <shuvayu.dhar@gmail.com>
2017-07-28 08:21:35 +1000

    Initial Commit

---
+++ ConsolePDF/PDFMethods.cs
@@ -0,0 +1,103 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePDF
+{
+    public class PDFMethods
+    {
+
+        /// <summary>
+        /// Get pdf files.
+        /// </summary>
+        static string[] GetFiles()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
+            FileInfo[] fileInfos = dirInfo.GetFiles("*.pdf");
+            ArrayList list = new ArrayList();
+            foreach (FileInfo info in fileInfos)
+            {                
+                if (info.Name.IndexOf("protected") == -1)
+                    list.Add(info.FullName);
+            }
+            return (string[])list.ToArray(typeof(string));
+        }
+
+        /// <summary>
+        /// Imports all pages from a list of documents.
+        /// </summary>
+        static void MergePdfs()
+        {
+            // Get some file names
+            string[] files = GetFiles();
+
+            // Open the output document
+            PdfDocument outputDocument = new PdfDocument();
+
+            // Iterate files
+            foreach (string file in files)
+            {
+                // Open the document to import pages from it.
+                PdfDocument inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+
+                // Iterate pages
+                int count = inputDocument.PageCount;
+                for (int idx = 0; idx < count; idx++)
+                {
+                    // Get the page from the external document...
+                    PdfPage page = inputDocument.Pages[idx];
+                    // ...and add it to the output document.
+                    outputDocument.AddPage(page);
+                }
+            }
+
+            // Save the document...
+            string filename = "ConcatenatedDocument.pdf";
+            outputDocument.Save(filename);
+            // ...and start a viewer.
+            Process.Start(filename);
+        }
+
+        /// <summary>
+        /// WIP
+        /// </summary>
+        static void RotatePdf()
+        {
+            // Get some file names
+            string[] files = GetFiles();
+
+            // Open the output document
+            PdfDocument outputDocument = new PdfDocument();
+
+            // Iterate files
+            foreach (string file in files)
+            {
+                // Open the document to import pages from it.
+                PdfDocument inputDocument = PdfReader.Open(file, PdfDocumentOpenMode.Import);
+
+                // Iterate pages
+                int count = inputDocument.PageCount;
+                for (int idx = 0; idx < count; idx++)
+                {
+                    // Get the page from the external document...
+                    PdfPage page = inputDocument.Pages[idx];
+                    // ...and add it to the output document.
+                    outputDocument.AddPage(page);
+                }
+            }
+
+            // Save the document...
+            string filename = "RotatedDocument.pdf";
+            outputDocument.Save(filename);
+            // ...and start a viewer.
+            Process.Start(filename);
+        }
+    }
+}
---
+++ ConsolePDF/Program.cs
@@ -0,0 +1,19 @@
+﻿using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PdfPage.PdfStream();
+            PdfDocument doc = new PdfDocument();
+            doc.Pages.Add();
+        }
+    }
+}
