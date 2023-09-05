50500a9ddb6ff35671b7666b3be9c3ad3af36d8a
Shuvayu Dhar <shuvayu.dhar@gmail.com>
2023-09-05 23:33:03 +1000

    Added compress functionality

--- ConsolePDF/Program.cs
+++ ConsolePDF/Program.cs
@@ -1,9 +1,4 @@
-﻿using PdfSharp.Pdf;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace ConsolePDF
 {
@@ -14,6 +9,7 @@ namespace ConsolePDF
             Console.WriteLine("Please seletc which option you want to proceed with ...");
             Console.WriteLine("[R]otate a pdf");
             Console.WriteLine("[M]erge pdfs");
+            Console.WriteLine("[C]ompress pdfs");
             char option = char.ToLower(Console.ReadKey().KeyChar);
 
             switch (option)
@@ -23,6 +19,9 @@ namespace ConsolePDF
                     break;
                 case 'm':
                     PDFMethods.MergePdfs();
+                    break;
+                case 'c':
+                    PDFMethods.CompressPdf();
                     break;
                 default:
                     Console.WriteLine("Next time select a valid option ...");
